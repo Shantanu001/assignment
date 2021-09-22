a22b961eabb4a8ce03fc79ef7afb596f6af5fbaf
Shantanu001 <sjshantanu001@gmail.com>
2021-09-23 01:46:56 +0530

    v1 deployed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { Container, Row, Col, Form, Card } from "react-bootstrap";
 function App() {
+  const [CityDetail, setCityDetail] = useState();
+  const [hdFlag, setHdEnabled] = useState();
+  const [oneWayFlag, setOneWayEnabled] = useState();
+  const [searchText, setSearchText] = useState();
+
+
+  useEffect(() => {
+    console.log(searchText);
+    const url = "https://api.zoomcar.com/v4/cities?platform=web";
+    Axios.get(url).then(function (response) {
+      console.log("response------>", response);
+      let CityDetail = response.data.cities.map((el) => {
+        let city = {};
+        city.name = el.name;
+        city.icon = el.icon;
+        city.popular = el.popular;
+        city.hd_enabled = el.hd_enabled;
+        city.one_way_enabled = el.one_way_enabled;
+        return city;
+      });
+      CityDetail = CityDetail.filter(el => {return el.hd_enabled === hdFlag && el.one_way_enabled === oneWayFlag && el.name.includes(searchText)})
+      setCityDetail(CityDetail);
+      console.log("to SHow===",CityDetail);
+    });
+  }, [hdFlag,oneWayFlag,searchText]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+          <Row>
+            {" "}
+            <span className="heading">CITIES</span>
+          </Row>
+          <Row>
+            <Form className="controlForm">
+              <Col>
+                <Form.Control size="sm" type="text" placeholder="Search City" onChange={(e)=>setSearchText(e.target.value)} />
+              </Col>
+              <Col className="filter">
+                <Form.Check type="checkbox" label="HD ENABLED" onChange={(e)=>setHdEnabled(e.target.checked)}/>
+                <Form.Check type="checkbox" label="ONE WAY ENABLED" onChange={(e)=>setOneWayEnabled(e.target.checked)}/>
+              </Col>
+            </Form>
+          </Row>
+          <Row className="cities">
+                {CityDetail &&
+                  CityDetail.map((city) => (
+                    <Col sm={4}>
+                      <Card className="card" >
+                        <Card.Img className="avatar" variant="top" src={city.icon} />
+                        <Card.Body>
+                          <Card.Title>{city.name}</Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ))}
+          </Row>
+      </Container>
     </div>
   );
 }
